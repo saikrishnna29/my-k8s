dc2e14db1dd56cee4ab3cf84acb2f7a4b83563b5
saikrishnna29 <97147477+saikrishnna29@users.noreply.github.com>
2022-03-21 11:09:41 +0530

    Update LokiStack.sh

--- loki-stack-loki-prometheus-grafana/LokiStack.sh
+++ loki-stack-loki-prometheus-grafana/LokiStack.sh
@@ -17,20 +17,25 @@ fi
 #Installing eksctl
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp && 
 sudo mv /tmp/eksctl /usr/local/bin
+if [ $? -eq "0" ]
+then
+echo "*** eksctl is installed successfully ***"
+else 
+echo "*** Failed to install eksctl ***"
+fi
 
 #Installing kubectl
 curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.21.2/2021-07-05/bin/linux/amd64/kubectl && 
-chmod +x ./kubectl && 
+chmod +x ./kubectl
+mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
+echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
 kubectl version --short --client 
-if [ $? -eq "0"]
+if [ $? -eq "0" ]
 then
 echo "*** kubectl is installed successfully ***"
 else
 echo "*** Failed to install kubectl ***"
 fi
-
-mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
-echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
 
 #Installing loki with helm
 helm repo add grafana https://grafana.github.io/helm-charts && 
