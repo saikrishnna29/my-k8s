0b839760cb9e0624b7309b0ad3b393fa4284b196
EC2 Default User <ec2-user@ip-10-0-16-72.ap-south-1.compute.internal>
2022-03-25 21:17:23 +0000

    changes to LokiStack

--- loki-stack-loki-prometheus-grafana/LokiStack.sh
+++ loki-stack-loki-prometheus-grafana/LokiStack.sh
@@ -17,11 +17,19 @@ fi
 #Installing eksctl
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp && 
 sudo mv /tmp/eksctl /usr/local/bin
+if [ $? -eq "0" ]
+then
+echo "*** eksctl is installed successfully ***"
+else
+echo "*** Failed to install eksctl ***"
+fi
 
 #Installing kubectl
 curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.21.2/2021-07-05/bin/linux/amd64/kubectl && 
-chmod +x ./kubectl && 
-kubectl version --short --client 
+chmod +x ./kubectl
+mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
+echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
+kubectl version --short --client
 if [ $? -eq "0"]
 then
 echo "*** kubectl is installed successfully ***"
@@ -29,11 +37,11 @@ else
 echo "*** Failed to install kubectl ***"
 fi
 
-mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
-echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
-
+#Setup k8s cluster
 #Installing loki with helm
 helm repo add grafana https://grafana.github.io/helm-charts && 
 helm repo update && 
 helm search repo grafana 
 helm show values grafana/loki-stack >> $PWD/loki-stack-values.yaml # Edit the values for custom options
+
+#grafana username-admin and password can be found by running kubectl get secret -n loki loki-stack-grafana -o jsonpath="{.data.admin-password}" | base64 --decode ; echo
